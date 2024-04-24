82950eea40f71dbecf48bd55f5a10d3774f7ecc4
MuraliKeloth <muralikknambiar@gmail.com>
2024-04-24 00:11:43 -0500

    Completed Model Evaluation

--- src/mlProject/components/model_trainer.py
+++ src/mlProject/components/model_trainer.py
@@ -17,14 +17,14 @@ class ModelTrainer:
         test_data = pd.read_json(self.config.test_data_path, lines=True)
 
 
-        #train_x = train_data.drop([self.config.target_column])
-        #test_x = test_data.drop([self.config.target_column])
-        #train_y = train_data[[self.config.target_column]]
-        #test_y = test_data[[self.config.target_column]]
-
-
-        #lr = ElasticNet(alpha=self.config.alpha, l1_ratio=self.config.l1_ratio, random_state=42)
+        train_x = train_data.drop([self.config.target_column], axis=1)
+        test_x = test_data.drop([self.config.target_column],axis=1)
+        
+        train_y = train_data[[self.config.target_column]]
+        test_y = test_data[[self.config.target_column]]
+        
+        lr = ElasticNet(alpha=self.config.alpha, l1_ratio=self.config.l1_ratio, random_state=42)
         #lr.fit(train_x, train_y)
 
-        #joblib.dump(lr, os.path.join(self.config.root_dir, self.config.model_name))
+        joblib.dump(lr, os.path.join(self.config.root_dir, self.config.model_name))
 
